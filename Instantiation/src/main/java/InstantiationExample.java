402fb63ef9aac89c751c6912a592995126159f34
joy432 <brooklily1sta@yahoo.com>
2023-01-04 22:41:33 +0000

    Saved progress on Instantiation 2023-01-04 22:41:33 +0000

--- Instantiation/src/main/java/InstantiationExample.java
+++ Instantiation/src/main/java/InstantiationExample.java
@@ -18,6 +18,9 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+
+        Object x = new Object ();
+        return x;
     }
+    
 }
