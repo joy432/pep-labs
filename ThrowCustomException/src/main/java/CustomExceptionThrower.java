ef133bd946d5351fe3a9e5fabcf8f1dc799260e3
joy432 <brooklily1sta@yahoo.com>
2023-01-14 01:36:26 +0000

    Saved progress on ThrowCustomException 2023-01-14 01:36:26 +0000

--- ThrowCustomException/src/main/java/CustomExceptionThrower.java
+++ ThrowCustomException/src/main/java/CustomExceptionThrower.java
@@ -1,6 +1,6 @@
 
 public class CustomExceptionThrower {
-
+   String animal="elephant";
     /**
      * This method should throw a CustomException. CustomException is a custom exception that we've written ourselves
      * by extending the Exception class in CustomException.java. In a large scale application, writing custom
@@ -8,24 +8,21 @@ public class CustomExceptionThrower {
      *
      * I recommend looking at the test case for a good example of try/catch block usage, as well.
      */
-    public void throwCustomException( )throws CustomException {
-
-        String animal = "elephant";
-
+    public void throwCustomException( ) throws CustomException{
+         
+            
         if (animal!="elephant"){
-
-            throw new CustomException();
             try{
-            System.out.println (animal= "panda");
-            } catch (CustomException){
-                System.out.println ("animal must be elephant!");
+            System.out.println (animal ="panda");
+            throw new CustomException();
+
+            } catch (CustomException a){
             }
         }else {
             System.out.println(animal);
         }
-
-        
-
+    } 
+    
     }
 
-}
+
