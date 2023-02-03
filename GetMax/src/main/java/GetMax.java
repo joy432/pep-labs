e1f4d25adbfa006f41c112e4f0d1750834a66e7c
joy432 <brooklily1sta@yahoo.com>
2023-02-03 06:17:11 +0000

    Saved progress on GetMax 2023-02-03 06:17:11 +0000

--- GetMax/src/main/java/GetMax.java
+++ GetMax/src/main/java/GetMax.java
@@ -1,3 +1,5 @@
+
+
 public class GetMax {
     /**
      * This method should return the max value of arr (the largest int.)
@@ -7,6 +9,17 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+
+        int max = arr[0];
+       
+        for (int i = 0; i<arr.length; i++){
+            if (arr[i]>=max){
+                max = arr[i];
+              
+            }
+
+        }
+
+        return  max;
     }
 }
