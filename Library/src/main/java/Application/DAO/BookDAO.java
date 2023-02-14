7656de21d4921af2053a2aab34c9238980332b8e
joy432 <brooklily1sta@yahoo.com>
2023-02-14 00:06:04 +0000

    Saved progress on Library 2023-02-14 00:06:04 +0000

--- Library/src/main/java/Application/DAO/BookDAO.java
+++ Library/src/main/java/Application/DAO/BookDAO.java
@@ -57,10 +57,11 @@ public class BookDAO {
         Connection connection = ConnectionUtil.getConnection();
         try {
             //Write SQL logic here
-            String sql = "select * from book where isbn = name";
+            String sql = "select * from book where isbn = ?";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
             //write preparedStatement's setInt method here.
+            preparedStatement.setInt(1, isbn );
 
             ResultSet rs = preparedStatement.executeQuery();
             while(rs.next()){
@@ -87,11 +88,14 @@ public class BookDAO {
         Connection connection = ConnectionUtil.getConnection();
         try {
             //Write SQL logic here
-            String sql = "insert into book (isbn)" ;
+            String sql = "insert into book (isbn, author_id,  title, copies_available) values (?,?,?,?)" ;
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
             //write preparedStatement's setString and setInt methods here.
-
+            preparedStatement.setInt(1, book.getIsbn());
+            preparedStatement.setInt (2, book.getAuthor_id());
+            preparedStatement.setString(3, book.getTitle());
+            preparedStatement.setInt(4, book.getCopies_available());
             preparedStatement.executeUpdate();
             return book;
         }catch(SQLException e){
@@ -109,7 +113,7 @@ public class BookDAO {
         List<Book> books = new ArrayList<>();
         try {
             //Write SQL logic here
-            String sql = "select * from book where ";
+            String sql = "select   ";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
             //write preparedStatement's setInt method here.
