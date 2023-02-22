0538ee963c3ac045a1c3b9cc077008a668129b11
joy432 <brooklily1sta@yahoo.com>
2023-02-22 03:32:54 +0000

    Saved progress on MostCommonCharacter 2023-02-22 03:32:54 +0000

--- StringIsPalindrome/src/main/java/Palindrome.java
+++ StringIsPalindrome/src/main/java/Palindrome.java
@@ -12,17 +12,10 @@ public class Palindrome {
      * @return true if str is a palindrome, false otherwise.
      */
     public boolean pal(String str){
-        String rev = "";
-        boolean ans = false;
-
-        for(int i = str.length() -1; 1>=0; i-- ){
-            rev = rev + str.charAt(i);
-        }
-            if (str.equals(rev)){
-            ans = true;
+        str = str.reverse();
         
         return false;
-            }
+            
     }
 
     
